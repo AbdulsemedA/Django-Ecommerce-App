2cdd7012b227c02acc127faf9ec03f2e7bc1fa5e
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-24 18:38:40 +0300

    Refactor admin.py files to comment out unused imports and enhance model registration in store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price','inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_select_related = ['collection']
+    list_per_page = 10
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "Ok"
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    ordering = ['first_name', 'last_name']
+    list_editable = ['membership']
+    list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer']
+    
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
