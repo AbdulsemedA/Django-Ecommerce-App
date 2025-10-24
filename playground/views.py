210e688c386997d968a0381bd222be6ec0af4175
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-24 10:42:21 +0300

    Update hello.html to display product collection titles and modify views.py to fetch products with related collections

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 # Create your views here.
 def calculate():
@@ -10,10 +10,23 @@ def calculate():
     return x
 
 def say_hello(request):
-    # query_set = Product.objects.all()
-    # queryset = Product.objects.filter(Q(inventory__lt=10) & Q(unit_price__lt=20))
-    queryset = Product.objects.filter(inventory=F('unit_price'))
+    # query_set1 = Product.objects.all()
+    
+    # queryset1 = Product.objects.filter(Q(inventory__lt=10) & Q(unit_price__lt=20))
+    # queryset2 = Product.objects.filter(inventory=F('unit_price'))
+    # queryset3 = Product.objects.order_by('unit_price', '-title')
+    # prod1 = Product.objects.order_by('unit_price')[0]
+    # prod2 = Product.objects.earliest('unit_price')
+    # query3 = Product.objects.values('id', 'title', 'collection__title')
+    # query4 = Product.objects.values_list('id', 'title', 'collection__title')
 
+# exercise, get all products that are ordered sorted by their titles
+
+    # query_set1 = Product.objects.filter(id__in = OrderItem.
+    # objects.values('product_id').distinct()).order_by('title')
+
+    # query_set1 = Product.objects.only('id', 'title')
+    # query_set1 = Product.objects.defer('description')
 
 
     # for product in query_set:
@@ -21,5 +34,7 @@ def say_hello(request):
     # try:
     # except ObjectDoesNotExist:
     #     pass
+
+    query_set1 = Product.objects.select_related('collection').all()
     
-    return render(request, 'hello.html', { 'name': 'mosh', 'products': list(queryset) })
+    return render(request, 'hello.html', { 'name': 'mosh', 'products': list(query_set1) })
