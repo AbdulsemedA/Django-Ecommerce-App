02018ab95c03051a9cd3994d383fe2a246f7b82b
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-25 10:18:20 +0300

    Enhance admin interface by adding InventoryFilter for low inventory items and updating ProductAdmin and CustomerAdmin to include relevant filters and counts.

--- store/admin.py
+++ store/admin.py
@@ -4,10 +4,25 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from . import models
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+# PRODUCT
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price','inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_select_related = ['collection']
     list_per_page = 10
 
@@ -20,6 +35,7 @@ class ProductAdmin(admin.ModelAdmin):
             return "Low"
         return "Ok"
 
+# CUSTOMER
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders']
@@ -45,10 +61,12 @@ class CustomerAdmin(admin.ModelAdmin):
             orders_count=Count('order')
         )
 
+# ORDER
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['placed_at', 'payment_status', 'customer']
-    
+
+# COLLECTION  
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
