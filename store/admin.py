2ed1bdb6f1be0585fda65674374712bdb78835b3
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-25 13:20:18 +0300

    Enhance admin interface by adding autocomplete and prepopulated fields to ProductAdmin and OrderAdmin, and update model options for Collection, Customer, and Product.

--- store/admin.py
+++ store/admin.py
@@ -20,6 +20,10 @@ class InventoryFilter(admin.SimpleListFilter):
 # PRODUCT
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price','inventory_status', 'collection_title']
     list_editable = ['unit_price']
@@ -75,11 +79,13 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['placed_at', 'payment_status', 'customer']
+    autocomplete_fields = ['customer']
 
 # COLLECTION  
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
